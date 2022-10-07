40c3cdeceea124e0e3a9842737e3156811bed34f
Julio Pereira <juliocezar.marketing@gmail.com>
2022-10-07 20:25:32 -0300

    exercicios para serem feitos

---
+++ main.py
@@ -0,0 +1,60 @@
+"""  
+    EXERCICIO 01
+    
+    Escrever um programa em Python que solicite informações de três pessoas, 
+    como nome, idade, peso e altura. Apresentar na tela estas informações de modo que permaneçam alinhados verticalmente. 
+    Usar a formatação de interpolação.
+
+"""
+
+"""
+    EXERCICIO 02
+    
+    Criar um programa que utilize uma estrutura de repetição for. Nesta estrutura, 
+    o usuário deverá fornecer 5 nomes a serem adicionados em uma lista. No final, apresentar os nomes em ordem crescente.
+    
+"""
+
+"""
+    EXERCICIO 03
+    Neste laboratório, uma lista de 100 números será criada de forma aleatória, 
+    ou seja, os elementos serão números aleatórios.
+    Escrever o programa de forma a exibir E adicionar em uma lista, apenas os valores gerados que sejam maiores que 10.
+    
+"""
+
+"""
+    EXERCICIO 04
+    
+    Escrever um programa que produza uma senha com 4 dígitos numéricos entre 0 e 9.
+    A senha resultante deve ser uma string.
+    
+    Observação: cada dígito gerado deve ser concatenado com o próximo.
+    
+"""
+
+"""
+    EXERCICIO 05
+
+    Neste laboratório, o usuário fornece as informações: dia, mês e ano.
+    Com base nestas informações, determinar quantos dias restam para terminar o ano informado.
+    Realizar as validações:
+    
+    => O mês informado deve estar na faixa entre 1 e 12;
+    => O dia informado deve ser compatível com o mês informado;
+    => Usar o ano para determinar o número de dias do mês de fevereiro. 
+"""
+
+"""
+    EXERCICIO 06
+    
+    Este programa representa um jogo para adivinhar números. Os passos são apresentados a seguir, com um exemplo de uso:
+    
+    => O programa produz um número aleatório inteiro entre 1 e 100 (suponha: 49); 
+    => O programa pede para o usuário: "Informe um valor entre 0 e 100": 60; 
+    => Como o número informado é maior que o produzido, o programe pede: "Informe um valor entre 0 e 59": 20; 
+    => Como o número informado é menor que o produzido, o programa pede: "Informe um valor entre 21 e 59"; 
+    => No final, o programa apresenta o número de tentativas.
+    
+    Observação: se o usuário fornece um valor fora da faixa solicitada, conta como tentativa. 
+"""
